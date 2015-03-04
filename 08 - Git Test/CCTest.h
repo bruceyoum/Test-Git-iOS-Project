9cd29e77d7fe68d14e8ec8b2b557c7295db303d3
Bruce Youm <bruceyoum@Bruces-MacBook-Pro.local>
2015-03-04 07:24:21 -0800

    added a test class

---
+++ 08 - Git Test/CCTest.h
@@ -0,0 +1,13 @@
+//
+//  CCTest.h
+//  08 - Git Test
+//
+//  Created by Bruce Youm on 3/4/15.
+//  Copyright (c) 2015 Bruce Youm. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CCTest : NSObject
+
+@end
---
+++ 08 - Git Test/CCTest.m
@@ -0,0 +1,13 @@
+//
+//  CCTest.m
+//  08 - Git Test
+//
+//  Created by Bruce Youm on 3/4/15.
+//  Copyright (c) 2015 Bruce Youm. All rights reserved.
+//
+
+#import "CCTest.h"
+
+@implementation CCTest
+
+@end
